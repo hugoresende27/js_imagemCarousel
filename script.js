00d90b690eb2fcf4ee0837e65932ffdb2a4a3f9c
hugoresende27 <Hugoresende27@gmail.com>
2021-11-19 11:13:59 +0000

    sex

---
+++ script.js
@@ -0,0 +1,26 @@
+const imagens = document.getElementById('imgs')
+const leftBtn = document.getElementById('esq')
+const rightBtn = document.getElementById('dir')
+
+const img = document.querySelectorAll('#imgs img')
+
+let index = 0
+
+let intervalo = setInterval(run, 2000)
+
+function run() {
+    index++
+
+    mudaImagem()
+}
+
+function mudaImagem() {
+    if (index > img.length-1)     //se o index for maior do que o total de img.lenght -1 = ultimo index, se estivermos no fim
+    {
+        index = 0       //index 0
+    } else if (index < 0) {//se estivermos no principio
+        index = img.length -1//index é o fim
+    }
+
+    imagens.style.transform = `translateX(${-index * 500}px)`//move a imagem 500px
+}
